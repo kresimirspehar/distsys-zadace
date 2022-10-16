44e2f5b92d9a09e194abe8f924ad78f944b13641
kresimirspehar <kresimirspehar5@gmail.com>
2022-10-16 14:48:32 +0200

    Ispravak zadatak3

--- 01-zadaci/zadatak3.py
+++ 01-zadaci/zadatak3.py
@@ -1,13 +1,8 @@
-#Funkcija uzima listu dictionary-a o artiklima. Provjerava je li parametar
-#lista, ak ne error. Provjerava jesu li svi elementi dictionary, ako ne error.
-#Provjerava imaju li svi dictionary odgovarajuća 3 ključeva, ako ne error.
-#(“cijena”,“naziv”,“kolicina”) (Moze i u dvije linije) Vraća novi nested
-#dictionary s ključem “ukupno” i dictionary sa ključem “artikli” i listom
-#svih odabranih artikala te “cijena” s ukupnom cijenom računa. (Ne treba
-#biti One-liner)
-#Ispis: [{“cijena”:8,“naziv”:“Kruh”,“kolicina”:1}, {“cijena”:13,“naziv”:“Sok”,“kolicina”:2},
-#{“cijena”:7,“naziv”:“Upaljac”,“kolicina”:1}] -> {‘ukupno’: {‘artikli’:
-#[‘Kruh’, ‘Sok’, ‘Upaljac’], ‘cijena’: 57}}
+#Funkcija uzima listu dictionary-a o artiklima. Provjerava je li parametar lista, ak ne error. Provjerava jesu li svi elementi dictionary, ako ne error.
+#Provjerava imaju li svi dictionary odgovarajuća 3 ključeva, ako ne error. (“cijena”,“naziv”,“kolicina”) (Moze i u dvije linije) Vraća novi nested
+#dictionary s ključem “ukupno” i dictionary sa ključem “artikli” i listom svih odabranih artikala te “cijena” s ukupnom cijenom računa. (Ne treba
+#biti One-liner) Ispis: [{“cijena”:8,“naziv”:“Kruh”,“kolicina”:1}, {“cijena”:13,“naziv”:“Sok”,“kolicina”:2},
+#{“cijena”:7,“naziv”:“Upaljac”,“kolicina”:1}] -> {‘ukupno’: {‘artikli’:[‘Kruh’, ‘Sok’, ‘Upaljac’], ‘cijena’: 57}}
 
 dic_list = [{"cijena":8, "naziv":"Kruh", "kolicina":1}, {"cijena":13, "naziv":"Sok", "kolicina":2}, {"cijena":7, "naziv":"Upaljac", "kolicina":1}]
 
