8f37d66354f70002af14c2387d16cdff4ce23c0d
kresimirspehar <kresimirspehar5@gmail.com>
2022-10-16 14:51:51 +0200

    Rjesen zadatak1

---
+++ 01-zadaci/zadatak1.py
@@ -0,0 +1,10 @@
+#Funkcija uzima listu string-ova. Provjeri dal su sve stringovi, ako ne error. #Vraća novu listu, gdje su string-ovi duži od 4 znaka. (Funkcija od dvije
+#linije) #Ispis: [“Pas”, “Macka”, “Stol”] -> [“Macka”]
+
+lista1 = ['Pas', 'Macka', 'Stol']
+
+def provjera(x):
+    assert all(isinstance(i, str) for i in x)
+    return [i for i in x if len(i) > 4]
+
+print(provjera(lista1))
